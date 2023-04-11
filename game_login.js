eada611b9392d283faab0f0c48d83f93996b28a5
Kokoadekwu <119619652+Kokoadekwu@users.noreply.github.com>
2023-04-11 18:00:29 +0100

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,13 @@
+// Create function addUser()
+  function addUser(){
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+    localStroage.setItem("player1_name", player1_name);
+    localStroage.setItem("player2_name", player2_name);
+
+    window.location = "game_page.html";
+}
+
+
+
